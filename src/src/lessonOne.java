dc50c35d09e8a5d7c7b8b65331105eebac98ddf8
AlikArm <alik_94_96@mail.ru>
2018-07-09 02:30:42 +0400

    first commit

---
+++ src/src/lessonOne.java
@@ -0,0 +1,16 @@
+public class lessonOne {
+
+    public static void main(String[] args) {
+        one(5,16);
+    }
+
+
+    //head 5
+    private static void  one(int N, int K){
+        if (N > 0){
+            for (int i = 0; i < N; i++) {
+                System.out.println(K);
+            }
+        }
+    }
+}
